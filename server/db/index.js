8ed68879535902d8f5932a46ab6990ef81587284
Bryan Gottschalk <bryanlgottschalk@gmail.com>
2019-10-16 13:22:07 -0500

    update gitignore

--- server/db/index.js
+++ server/db/index.js
@@ -1,21 +1,13 @@
-// The purpose of this module is to bring your Sequelize instance (`db`) together
-// with your models, for which you'll find some blank files in this directory:
+// bring DB together with models and associations
 
 const db = require('./database');
 const Student = require('./student');
 const Campus = require('./campus');
 
-// This is a great place to establish associations between your models
-// (https://sequelize-guides.netlify.com/association-types/).
-// Example:
-//
-// Puppy.belongsTo(Owner)
-
 Campus.hasMany(Student);
 Student.belongsTo(Campus);
 
 module.exports = {
-  // Include your models in this exports object as well!
   db,
   Student,
   Campus,
